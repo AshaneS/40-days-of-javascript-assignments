cb728d77eba05bf642628a1c75b3104f2cbfc902
Ashane Silva <fatpuppyaudio@gmail.com>
2026-04-22 23:12:53 +0530

    switched to the ternary operator

--- DAY-03/01-ODD OR EVEN/index.js
+++ DAY-03/01-ODD OR EVEN/index.js
@@ -1,7 +1,11 @@
-const number = 0;
+const number = 1023;
 
-if (number % 2 === 1) {
-  console.log(`${number} is odd`);
-} else {
-  console.log(`${number} is even`);
-}
+// if (number % 2 === 1) {
+//   console.log(`${number} is odd`);
+// } else {
+//   console.log(`${number} is even`);
+// }
+
+// using the ternary operator
+const result = (number % 2 == 1) ? `${number} is odd` : `${number} is even`;
+console.log(result);
