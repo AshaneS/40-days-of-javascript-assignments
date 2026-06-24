5efb70c5e83d46871e1c94f38fa62c0cd44005bc
Ashane Silva <fatpuppyaudio@gmail.com>
2026-06-24 23:30:36 +0530

    day04 - task 02

---
+++ DAY-04/02 - ATM/index.js
@@ -0,0 +1,10 @@
+const amount = 1860;
+
+const ifMultiplesOfHundred = amount % 100;
+
+if(ifMultiplesOfHundred == 0){
+ console.log("Withdrawal Successful")
+}
+else {
+ console.log("Invalid Amount")
+}
