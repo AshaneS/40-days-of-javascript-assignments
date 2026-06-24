90611ebd35b97735a81fd71e136706cd021799d8
Ashane Silva <fatpuppyaudio@gmail.com>
2026-06-24 23:32:41 +0530

    day04 - task 02 fixed comparison operator

--- DAY-04/02 - ATM/index.js
+++ DAY-04/02 - ATM/index.js
@@ -2,7 +2,7 @@ const amount = 1860;
 
 const ifMultiplesOfHundred = amount % 100;
 
-if(ifMultiplesOfHundred == 0){
+if(ifMultiplesOfHundred === 0){
  console.log("Withdrawal Successful")
 }
 else {
