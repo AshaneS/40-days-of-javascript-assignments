151a3005761a29dd21a88ea92ec85198ce588e52
Ashane Silva <fatpuppyaudio@gmail.com>
2026-06-25 19:32:53 +0530

    day04 - task 02 chaned variable name to divisible

--- DAY-04/02 - ATM/index.js
+++ DAY-04/02 - ATM/index.js
@@ -1,8 +1,8 @@
 const amount = 1860;
 
-const ifMultiplesOfHundred = amount % 100;
+const divisible = amount % 100;
 
-if(ifMultiplesOfHundred === 0){
+if(divisible === 0){
  console.log("Withdrawal Successful")
 }
 else {
