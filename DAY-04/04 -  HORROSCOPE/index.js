03bb3ae85bdeab1a38c447e65e1a81cc281ee0b1
Ashane Silva <fatpuppyaudio@gmail.com>
2026-06-25 21:41:56 +0530

    day4 - task 4

---
+++ DAY-04/04 -  HORROSCOPE/index.js
@@ -0,0 +1,41 @@
+const startMonth = "December"
+const endMonth = "January"
+
+switch (startMonth + " - " + endMonth) {
+  case "January - February":
+    console.log("zodiac sign is Aquarius");
+    break;
+  case "February - March":
+    break;
+    console.log("zodiac sign is Pisces");
+  case "March - April":
+    console.log("zodiac sign is Aries");
+    break;
+  case "April - May":
+    console.log("zodiac sign is Taurus");
+    break;
+  case "May - June":
+    console.log("zodiac sign is Gemini");
+    break;
+  case "June - July":
+    console.log("zodiac sign is Cancer");
+    break;
+  case "July - August":
+    console.log("zodiac sign is Leo");
+    break;
+  case "August - Sepetember":
+    console.log("zodiac sign is Virgo");
+    break;
+  case "September - October":
+    console.log("zodiac sign is Libra");
+    break;
+  case "October - November":
+    console.log("zodiac sign is Scorpio");
+    break;
+  case "november - December":
+    console.log("zodiac sign is Sagittarius ");
+    break;
+  case "December - January":
+    console.log("zodiac sign is Capricorn");
+    break;
+}
