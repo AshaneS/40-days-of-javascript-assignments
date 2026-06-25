0220652ee8da5da085fbc67df99754d64ac145dd
Ashane Silva <fatpuppyaudio@gmail.com>
2026-06-25 21:42:56 +0530

    day4 - task 5

---
+++ DAY-04/05 -  MOVIE TICKET/index.js
@@ -0,0 +1,9 @@
+const age = 61;
+
+if (age < 18) {
+  console.log("Ticket price is 3");
+} else if (age > 60) {
+  console.log("Ticket price is 8");
+} else {
+  console.log("Ticket price is 10");
+}
