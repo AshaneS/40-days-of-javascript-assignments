5aee3a98387dfe8da58158c4919ff8ca28940cdf
Ashane Silva <fatpuppyaudio@gmail.com>
2026-07-08 22:58:42 +0530

    init: day 6 task 5

---
+++ DAY-06/05_COUNT_VOWELS/index.js
@@ -0,0 +1,23 @@
+function countVowels(str) {
+  let vowelSum = 0;
+  let position = 0;
+  while (position < str.length) {
+    if (
+      str[position] === "a" ||
+      str[position] === "e" ||
+      str[position] === "i" ||
+      str[position] === "o" ||
+      str[position] === "u"
+    ) {
+      vowelSum += 1;
+    }
+    position++;
+  }
+  return vowelSum;
+}
+
+console.log(
+  countVowels(
+    "the quick brown fox jumps over the lazy dog while eating a juicy orange",
+  ),
+);
