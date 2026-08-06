9dcc6b30d2663a45e6143bffe5d3250136e19b20
Ashane Silva <fatpuppyaudio@gmail.com>
2026-08-06 21:13:58 +0530

    day 9 task 2

--- DAY-09/02 -HOISTING/hoist.js
+++ DAY-09/02 -HOISTING/hoist.js
@@ -1,8 +1,28 @@
-let name = "global";
+let a = 10;
+const b = 15;
+
+let sum;
+test();
 
 function test() {
-  console.log(name); // Throws ReferenceError!
-  // let name = "local";
+  sum = a + b;
+  console.log("The sum is", sum);
 }
 
-test();
+/*
+ GEC creation process
+variable a, b, sum are hoisted but unintialized(because they are let and const) 
+ if the variables are defined with var - they are hoisted and initilized as undefined.
+the test() function declaration will be fully hoisted. 
+
+ during the Execution Process
+ a = 10
+ b = 15
+
+ FEC for test()
+ sum = 25
+ prints The sum is 25
+
+ Hoisting is JavaScript's behavior of allocating memory for variable and function declarations during the Creation Phase before any code is executed.
+var variables are hoisted and initialized to undefined, and let/const variables are hoisted but left uninitialized in the Temporal Dead Zone until their declaration line executes.
+*/
