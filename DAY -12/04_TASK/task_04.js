419c22f36217efaddff1df5de64ccfc79b619088
Ashane Silva <fatpuppyaudio@gmail.com>
2026-08-25 23:11:40 +0530

    init: day 12 task 4

--- DAY -12/04_TASK/task_04.js
+++ DAY -12/04_TASK/task_04.js
@@ -1,3 +1,20 @@
-const user = { name: "Alex", age: undefined };
-console.log(user.age ?? "Not provided");
+//Store student details in an object (name, age, grades).
+//Implement a method to calculate the average grade.
 
+const student = {
+  name: " John Doe",
+  age: 12,
+  grades: [88, 75, 66, 52, 63],
+  average: function () {
+    let sum = 0;
+    let average = 0;
+    for (let i = 0; i < this.grades.length; i++) {
+      sum += this.grades[i];
+    }
+    return (average = sum / this.grades.length);
+    console.log(average);
+  },
+};
+
+const studentAverage = student.average();
+console.log("student average is", studentAverage);
