df525fe0c85e091cd6ca5c05ad51a3aff7f63424
Ashane Silva <fatpuppyaudio@gmail.com>
2026-08-25 22:12:55 +0530

    init: day 12 task 3

--- DAY -12/03_TASK/task_03.js
+++ DAY -12/03_TASK/task_03.js
@@ -1,3 +1,22 @@
-const user = { name: "Alex", age: undefined };
-console.log(user.age ?? "Not provided");
+const person = {
+  name: "Tapas",
+  company: {
+    name: "tapaScript",
+    location: {
+      city: "Bangalore",
+      zip: "94107",
+    },
+  },
+};
 
+
+// answer
+
+const {
+  name,
+  company: {
+    name: companyName,
+    location: { city },
+  },
+} = person;
+console.log(name, companyName, city);
