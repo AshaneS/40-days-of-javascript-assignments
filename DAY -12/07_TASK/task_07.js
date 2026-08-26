bc1d05e5b00c2d067b0744c368bd6e6b88cece36
Ashane Silva <fatpuppyaudio@gmail.com>
2026-08-26 11:14:05 +0530

    init: day 12 task 7

--- DAY -12/07_TASK/task_07.js
+++ DAY -12/07_TASK/task_07.js
@@ -1,3 +1,13 @@
-const user = { name: "Alex", age: undefined };
-console.log(user.age ?? "Not provided");
+//How do you check if an object has a certain property?
 
+//answer -  we can use the In operator
+
+//example
+const employee = {
+  name: "john doe",
+  age: 39,
+  salary: "$8900",
+};
+
+console.log("salary" in employee); //true
+console.log("address" in employee); //false
