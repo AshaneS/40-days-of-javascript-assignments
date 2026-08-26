08a04a369534a0b7552092aaf12a7403ce4812c6
Ashane Silva <fatpuppyaudio@gmail.com>
2026-08-26 13:56:48 +0530

    init: day 12 task 1

--- DAY -12/01_TASK/task_01.js
+++ DAY -12/01_TASK/task_01.js
@@ -1,3 +1,15 @@
 const user = { name: "Alex", age: undefined };
 console.log(user.age ?? "Not provided");
 
+// answer
+
+/*
+
+user.age is undefined. so the returned value will be "Not Provided".
+
+
+with nullish coalescing operator, 
+ -- if the first expression is null or undefined return the second
+ -- if the first expression is neither null nor undefined returns the first. 
+
+ */
