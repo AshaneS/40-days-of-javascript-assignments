0f7037b4cea31c3286bb6d1f7b15d062de9eb3bb
Ashane Silva <fatpuppyaudio@gmail.com>
2026-08-26 11:07:07 +0530

    init day 12 task 6

--- DAY -12/06_TASK/task_06.js
+++ DAY -12/06_TASK/task_06.js
@@ -1,3 +1,14 @@
-const user = { name: "Alex", age: undefined };
-console.log(user.age ?? "Not provided");
+//What is the difference between Object.keys() and Object.entries()?
+// Explain with examples
 
+//Object.Entries will transfer the object into an array and Object.keys will give only the keys in the object
+
+const employee = {
+  name: "john doe",
+  age: "45",
+  company: "ABC company",
+  salary: "$3000",
+};
+
+console.log(Object.entries(employee)); // will convert the employee object into an Array
+console.log(Object.keys(employee)); // will print all keys in the object
