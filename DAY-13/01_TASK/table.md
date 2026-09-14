846319ebc57f0a311bf819cb162add773b4ba283
Ashane Silva <fatpuppyaudio@gmail.com>
2026-09-14 13:50:04 +0530

    day 13 task 1

---
+++ DAY-13/01_TASK/table.md
@@ -0,0 +1,10 @@
+1. Create a table of two columns, situation and value. Now add the rows for every situations and the value of this in that situation. Please cover the following situations
+- At the Global
+- Inside an Object Method
+- Inside the Satandalone non-Arrow Function
+- Inside an Arrow Function(standalone)
+- Inside an Arrow Function(as object method)
+- Inside an object created with the Constructor Function
+- Please add examples for each of the scenarios.
+
+![alt text](Untitled-2026-06-30-1208.png)
--- DAY-13/05_TASK/05_task.js
+++ DAY-13/05_TASK/05_task.js
@@ -3,18 +3,41 @@ const Sport = function (name, numberOfPlayers) {
   this.numberOfPlayers = numberOfPlayers;
 
   this.logDetails = function () {
-    console.log(`A ${this.name} team has ${this.numberOfPlayers} players`)
-  }
+    console.log(`A ${this.name} team has ${this.numberOfPlayers} players`);
+  };
 };
 
 const cricketSport = new Sport("cricket", 11);
 const basketballSport = new Sport("basketball", 5);
 
-
 //cricket team
-cricketSport.logDetails()
+cricketSport.logDetails();
 //basketball team
-basketballSport.logDetails()
+basketballSport.logDetails();
 
+console.log("I'm window", this);
 
+const user = {
+  userName: "Alex",
+  age: "35",
+  getBio: function () {
+    return ()=> console.log(this.userName, this.age);
+  },
+};
 
+user.getBio()();
+
+function one(a) {
+  console.log("function one", this);
+  function two(b) {
+    console.log("function two", this);
+  }
+  two();
+}
+
+one(12);
+
+("use strict");
+
+const getCar = () => console.log("arrow function in global scope", this);
+getCar();
